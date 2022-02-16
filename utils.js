f235dbc7d9181c25337117131135ae9314924af3
teachable-ainize <79987713+teachable-ainize@users.noreply.github.com>
2022-02-17 04:46:12 +0900

    Update utils.js

--- utils.js
+++ utils.js
@@ -4,12 +4,12 @@ const getTransactionHash = (transaction) => {
 	return '0x' + ainUtil.hashTransaction(transaction).toString('hex');
 }
 
-const getAddress = (hash, signature) => {
+const getAddress = (hash, signature, chainId) => {
 	const sigBuffer = ainUtil.toBuffer(signature);
 	const len = sigBuffer.length;
 	const lenHash = len - 65;
 	const { r, s, v } = ainUtil.ecSplitSig(sigBuffer.slice(lenHash, len));
-	const publicKey = ainUtil.ecRecoverPub(Buffer.from(hash.slice(2), 'hex'), r, s, v);
+	const publicKey = ainUtil.ecRecoverPub(Buffer.from(hash.slice(2), 'hex'), r, s, v, chainId);
 	return ainUtil.toChecksumAddress(ainUtil.bufferToHex(
 		ainUtil.pubToAddress(publicKey, publicKey.length === 65)));
 }
